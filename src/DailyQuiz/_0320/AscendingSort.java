74a5895fa392dd49573df83be9cc497c24114524
rma4921 <rma4921@naver.com>
2025-03-20 17:26:40 +0900

    add DailyQuiz 0320

---
+++ src/DailyQuiz/_0320/AscendingSort.java
@@ -0,0 +1,65 @@
+package DailyQuiz._0320;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/*
+ * 스트림과 컬렉션을 사용하여 코드를 작성해주세요.
+ * 문제는 스트림으로 해결한 버전과 스트림으로 해결하지 않은 버전 모두 작성해주세요.
+ * */
+public class AscendingSort {
+    public int[] solution(int[] array1, int[] array2) {
+        int[] result = new int[array1.length + array2.length];
+
+        // 코드 작성
+        /* 스트림으로 풀지 않는 경우.
+        1.
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i];
+        }
+        for (int i = 0; i < array2.length; i++) {
+            result[array1.length + i] = array2[i];
+        }
+        Arrays.sort(result);
+
+        2. 리스트로 푸는 방법
+        List<Integer> result1 = new ArrayList<>();
+        for (int num : array1) {
+            result.add(num);
+        }
+        for (int num : array2) {
+            result.add(num);
+        }
+        */
+        result = IntStream.concat(Arrays.stream(array1), Arrays.stream(array2))
+                .sorted()
+                .toArray();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        AscendingSort ascendingSort = new AscendingSort();
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int number2 = scanner.nextInt();
+        int[] array2 = new int[number2];
+
+        for (int i = 0; i < number2; i++) {
+            array2[i] = scanner.nextInt();
+        }
+
+        for (int x : ascendingSort.solution(array, array2)) {
+            System.out.print(x + " ");
+        }
+    }
+}
---
+++ src/DailyQuiz/_0320/PrintMoreBigNumber.java
@@ -0,0 +1,61 @@
+package DailyQuiz._0320;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/*
+* 스트림과 컬렉션을 사용하여 코드를 작성해주세요.
+* 문제는 스트림으로 해결한 버전과 스트림으로 해결하지 않은 버전 모두 작성해주세요.
+*
+* 입력설명
+* 첫 줄에 자연수 N이 주어지고, 그 다음 줄에 N개의 정수가 입력됩니다.
+*
+* 출력설명
+* 자신의 바로 앞 수보다 큰 수만 한 줄로 출력합니다.
+*
+* 입력예제 1
+* 6
+* 7 3 9 5 6 12
+*
+* 출력예제 1
+* 7 9 6 12
+* */
+
+public class PrintMoreBigNumber {
+    public List<Integer> solution(int n, int[] array) {
+        List<Integer> answer = new ArrayList<>();
+
+        // 코드 작성
+        answer.add(array[0]);
+        /* 스트림을 사용하지 않은 버전
+        for (int i = 1; i < n; i++) {
+            if (array[i] > array[i - 1]) {
+                answer.add(array[i]);
+            }
+        }
+        */
+        IntStream.range(1, n)
+                .filter(index -> array[index] > array[index - 1])
+                .forEach(index -> answer.add(array[index]));
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PrintMoreBigNumber printMoreBigNumber = new PrintMoreBigNumber();
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        for (int x : printMoreBigNumber.solution(number, array)) {
+            System.out.print(x + " ");
+        }
+    }
+}
